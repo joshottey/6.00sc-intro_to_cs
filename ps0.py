2d9efed1a745cb9b9ce25c6dcb3ae65287d25946
joshottey <joshua.ottey@gmail.com>
2019-03-04 20:13:21 -0500

    First commit

---
+++ ps0.py
@@ -0,0 +1,10 @@
+# Problem Set 0
+# Name: Josh Ottey
+# Collaborators: none
+# Time Spent: 00:25
+
+print "Enter your date of birth (mm/dd/yyyy):"
+dob = raw_input()
+print "Enter your last name:"
+lastName = raw_input()
+print lastName + ' ' + dob
